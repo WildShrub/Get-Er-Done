a5161d81fcdb258ed7969c69adf06dac00074840
WildShrub <erichampson825@gmail.com>
2025-11-25 20:27:04 -0600

    added some sql tables

---
+++ backend/database/schema.sql
@@ -0,0 +1,29 @@
+CREATE TABLE userSchedules (
+    username VARCHAR(50) PRIMARY KEY NOT NULL,
+    userSchedule JSON
+);
+
+CREATE TABLE userSchedules (
+    username VARCHAR(50) PRIMARY KEY NOT NULL,
+    userSchedule JSON
+);
+
+CREATE TABLE userSchedules (
+    username VARCHAR(50) PRIMARY KEY NOT NULL,
+    userSchedule JSON
+);
+
+CREATE TABLE userSchedules (
+    username VARCHAR(50) PRIMARY KEY NOT NULL,
+    userSchedule JSON
+);
+
+CREATE TABLE userSchedules (
+    username VARCHAR(50) PRIMARY KEY NOT NULL,
+    userSchedule JSON
+);
+
+CREATE TABLE userSchedules (
+    username VARCHAR(50) PRIMARY KEY NOT NULL,
+    userSchedule JSON
+);
